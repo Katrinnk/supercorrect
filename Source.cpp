46cbcac1c5385d6038ab984d6899fbd2a05f7651
Katrinnk <katerin69llobster@gmail.com>
2020-10-12 21:07:12 +0300

    lab2.2.2

--- Source.cpp
+++ Source.cpp
@@ -9,17 +9,17 @@ using namespace std;
 int main()
 {
 	double a;
-	double z1;
-	//double z2;
+	//double z1;
+	double z2;
 
 	cout << "a = "; cin >> a;
 
-	z1 = (sin(4.0 * a) / (1 + cos(4.0 * a)) * (cos(2.0 * a)) / (1.0 + cos(2.0 * a)));
-	//z2 = (1.0 / (tan(((3.0 / 2.0) * 4 * atan(1.0)) - a)));
+	//z1 = (sin(4.0 * a) / (1 + cos(4.0 * a)) * (cos(2.0 * a)) / (1.0 + cos(2.0 * a)));
+	z2 = (1.0 / (tan(((3.0 / 2.0) * 4 * atan(1.0)) - a)));
 
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	//cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
